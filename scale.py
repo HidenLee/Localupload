12d1a74dd167f103ca93d2447ee6b3c54c495122
HidenLee <hyundo1995@gmail.com>
2023-01-16 17:52:58 +0900

    0116_17:52

--- scale.py
+++ scale.py
@@ -1,8 +1,27 @@
-def scale(list1,t):
-    if list1 == []:  
-        return t
-    print(list1.pop())
-    scale(list1,t)    # 다시 hello에 넣음
-
-mugechu = [34 , 22, 11]
-scale(mugechu,2)
+import copy
+countnum = 0
+def scale(numlist,ltotal,rtotal):
+    global countnum
+    if rtotal > ltotal:
+        print(numlist,ltotal,rtotal,"fail",countnum)
+        return
+    if numlist == []:
+        countnum += 1
+        print(numlist,ltotal,rtotal,"end",countnum)  
+        return 1 
+    for i in range(len(numlist)):   
+        localt = numlist[i]
+        newlist = copy.deepcopy(numlist)
+        print("keep",newlist,ltotal,rtotal)   
+        newlist.remove(localt)
+        scale(newlist,ltotal+localt,rtotal)
+        scale(newlist,ltotal,rtotal+localt)
+        newlist.append(localt)   
+    return countnum       
+T = int(input())
+for test_case in range(T):
+    linnum= int(input())
+    target = list(map(int,input().split()))
+    scale(target,0,0)
+    print(f'#{test_case+1} {countnum}')
+    countnum = 0 
